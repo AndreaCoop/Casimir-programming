15df2577c74bf317ad9c9b6160657d8c08ceae63
AndreaCoop <andrea.cupertino@gmail.com>
2019-11-06 14:13:33 +0100

    add docstring to surface area

--- test.py
+++ test.py
@@ -3,5 +3,6 @@ print('Hello world')
 def circumference (radius):
 	return 2*radius*3.14
 
-def surf_area(radius)
+def surf_area(radius):
+	'this function calculates the surface area'
 	return (radius**2)*3.14
